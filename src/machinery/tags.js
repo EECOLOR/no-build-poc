3d08b45e3f5cd084e1061349bbf91b4031cb792d
EECOLOR <eecolor@qirx.org>
2024-06-16 16:14:31 +0200

    Improve tags

--- src/machinery/tags.js
+++ src/machinery/tags.js
@@ -1,7 +1,10 @@
 // TODO: escape html
 /** @typedef {((...children: Array<string>) => string) | ((attributes: object, ...children: Array<string>) => string)} Tag */
 
-/** @returns {{ html: Tag, head: Tag, body: Tag, div: Tag, script: Tag }} */
+import { writeToDom } from './domInteraction.js'
+
+const emptyValues = [false, undefined, null]
+
 export const tags = new Proxy(/** @type {any} */ ({}), {
   get(_, /** @type {string} */ tagName) {
     return function tag(attributesOrChild = {}, ...otherChildren) {
@@ -22,38 +25,102 @@ function renderTag(tagName, attributes, children) {
 }
 
 function renderServerTag(tagName, attributes, children) {
-  return `<${[tagName].concat(renderServerAttributes(attributes)).join(' ')}>${children.join('')}</${tagName}>`
+  return [
+    `<${[tagName].concat(renderServerAttributes(attributes)).join(' ')}>`,
+      asString(children),
+    `</${tagName}>`,
+  ].join('')
+}
+
+function renderServerAttributes(attributes) {
+  return Object.entries(attributes).flatMap(([k, v]) => {
+    if (k.startsWith('on')) return []
+    const value = isSignal(v) ? v.get() : v
+    return `${k}="${value}"`
+  })
+}
+
+function asString(value) {
+  return (
+    emptyValues.includes(value) ? '' :
+    Array.isArray(value) ? value.map(asString).join('') :
+    isSignal(value) ? asString(value.get()) :
+    String(value)
+  )
 }
 
 function renderClientTag(tagName, attributes, children) {
   /** @type {HTMLElement} */
   const element = document.createElement(tagName)
+
   Object.entries(attributes).forEach(([k, v]) => {
     if (k.startsWith('on')) element[k.toLowerCase()] = v
+    else if (v?.get) bindSignalToAttribute(element, k, v)
+    else if (k === 'style') Object.assign(element.style, v)
     else element.setAttribute(k, v)
   })
+
   children.forEach(child => {
-    const node = (
-      typeof child === 'string' ? document.createTextNode(child) :
-      child?.get ? signalAsNode(child) :
-      child
-    )
-    element.appendChild(node)
+    const nodes = asNodes(child)
+    nodes.forEach(node => element.appendChild(node))
   })
   return element
 }
 
-/** @param {import('/machinery/signal.js').Signal<any>} signal */
-function signalAsNode(signal) {
-  let node = document.createTextNode(signal.toString())
-  const unsubscribe = signal.subscribe(newValue => {
-    const newNode = document.createTextNode(String(newValue))
-    node.replaceWith(newNode)
-    node = newNode
-  })
-  return node
+function bindSignalToAttribute(element, attribute, signal) {
+  element.setAttribute(attribute, signal.get())
+
+  // TODO: unsubscribe when element is removed
+  const unsubscribe = signal.subscribe(value =>
+    writeToDom.outsideAnimationFrame(() => {
+      element.setAttribute(attribute, value)
+    })
+  )
 }
 
-function renderServerAttributes(attributes) {
-  return Object.entries(attributes).map(([k, v]) => `${k}="${v}"`)
+/** @param {import('/machinery/signal.js').Signal<any>} signal */
+function signalAsNodes(signal) {
+  const marker = comment()
+  let nodes = [marker, ...asNodes(signal.get())]
+
+  // TODO: unsubscribe when element is removed
+  const unsubscribe = signal.subscribe(newValue => {
+    const newNodes = asNodes(newValue)
+    const oldNodes = nodes.slice(1)
+
+    swapNodes(marker, newNodes, oldNodes)
+
+    nodes.splice(1, Infinity, ...newNodes)
+  })
+
+  return nodes
+}
+
+function swapNodes(marker, newNodes, oldNodes) {
+  writeToDom.outsideAnimationFrame(() => {
+    const lastNode = newNodes[oldNodes.length - 1] || marker
+    oldNodes.forEach((node, i) => { node.replaceWith(newNodes[i]) })
+    const leftOverNewNodes = newNodes.slice(oldNodes.length)
+    lastNode.after(...leftOverNewNodes)
+  })
+}
+
+function comment() {
+  return document.createComment('')
+}
+
+/** @returns {Array<ChildNode>} */
+function asNodes(value) {
+  return (
+    emptyValues.includes(value) ? [] :
+    Array.isArray(value) ? value.flatMap(asNodes) :
+    value instanceof Element ? [value] :
+    isSignal(value) ? signalAsNodes(value) :
+    [document.createTextNode(String(value))]
+  )
+}
+
+/** @type {(value: Object) => value is import('./signal.js').Signal<any>}*/
+function isSignal(value) {
+  return value.get && value.subscribe
 }
