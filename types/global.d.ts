05c914e0556bc437312d7b4fb4f77acb9ab07d48
EECOLOR <eecolor@qirx.org>
2024-06-29 15:57:05 +0200

    Improve typescript signatures

--- types/global.d.ts
+++ types/global.d.ts
@@ -8,3 +8,8 @@ declare namespace React {
     'data-banana'?: string
   }
 }
+
+declare interface ObjectConstructor {
+  entries<T extends { [s: string]: any } | ArrayLike<any>, Value extends T[keyof T]>(o: T): Array<[keyof T, Value]>
+  fromEntries<Key extends PropertyKey, Value>(entries: Iterable<readonly [Key, Value]>): { [k in Key]: Value }
+}
