f5e575e0a9e3713f8cb3a54f58c9ce4862a96511
EECOLOR <eecolor@qirx.org>
2024-06-29 12:34:29 +0200

    Utilize esbuild to prefix the class names

--- magic/hooks/css.js
+++ magic/hooks/css.js
@@ -1,6 +1,7 @@
 import fs from 'node:fs'
 import { fileURLToPath } from 'node:url'
-import crypto from 'node:crypto'
+import path from 'node:path'
+import * as esbuild from 'esbuild'
 
 /** @type {import('node:worker_threads').MessagePort} */
 let processedCssPort = null
@@ -17,9 +18,12 @@ export function initialize(data) {
 export async function load(url, context, nextLoad) {
   if (url.endsWith('.css')) {
     const filePath = fileURLToPath(url)
+    const prefix = path.relative('./src', filePath).replaceAll(/\//g, '_').slice(0, -4)
     const source = fs.readFileSync(filePath, 'utf8')
 
-    const { classMap, modifiedSource } = suffixClasses(source, computeHash(url))
+    const modifiedSource = await prefixClasses(prefix, source)
+    const classMap = createClassMap(prefix, modifiedSource)
+
     const classMapAsJs = `export default ${JSON.stringify(classMap)}`
 
     processedCssPort.postMessage({ url, modifiedSource, classMapAsJs })
@@ -30,20 +34,23 @@ export async function load(url, context, nextLoad) {
   return nextLoad(url, context)
 }
 
-// TODO: use css parser
-function suffixClasses(source, suffix) {
-  const classMap = {}
-  const modifiedSource = source.replaceAll(/\.([\w]+)/g, (_, className) => {
-    const replacement = `${className}-${suffix}`
-    classMap[className] = replacement
-    return `.${replacement}`
+async function prefixClasses(prefix, source) {
+  const { code } = await esbuild.transform(source, {
+    sourcefile: `${prefix}.css`,
+    loader: 'local-css',
+    // https://en.wikipedia.org/wiki/Timeline_of_web_browsers#2020s
+    target: ['chrome94', 'opera79', 'edge94', 'firefox92', 'safari15'],
   })
-
-  return { classMap, modifiedSource }
+  return code
 }
 
-function computeHash(content) {
-  const hash = crypto.createHash('md5')
-  hash.write(content)
-  return hash.digest('hex')
+function createClassMap(prefix, source) {
+  const classRegExp = new RegExp(String.raw`${prefix}_([\w]+)`, 'g')
+  const classMap = {}
+  let match
+  while (match = classRegExp.exec(source)) {
+    const [replacement, className] = match
+    classMap[className] = replacement
+  }
+  return classMap
 }
