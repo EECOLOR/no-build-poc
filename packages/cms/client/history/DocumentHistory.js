69ae2cea73f91bd75f2d353c707e85632045f1cd
EECOLOR <eecolor@qirx.org>
2025-04-04 12:30:35 +0200

    Experiment for showing history of rich text

--- packages/cms/client/history/DocumentHistory.js
+++ packages/cms/client/history/DocumentHistory.js
@@ -1,8 +1,12 @@
 import { derive } from '#ui/dynamic.js'
-import { css, tags } from '#ui/tags.js'
-import { getPathInfo, getSchema } from '../context.js'
+import { css, raw, tags } from '#ui/tags.js'
+import { Node } from 'prosemirror-model'
+import { context, getPathInfo, getSchema } from '../context.js'
 import { useEventSourceAsSignal } from '../machinery/useEventSourceAsSignal.js'
 import { ListSignal } from '../ui/List.js'
+import { AddMarkStep, Mapping, RemoveMarkStep, ReplaceStep, Step } from 'prosemirror-transform'
+import { Decoration, DecorationSet, EditorView } from 'prosemirror-view'
+import { EditorState, Plugin, PluginKey } from 'prosemirror-state'
 
 const { div, span, pre, code, del, ins, time, em } = tags
 
@@ -105,13 +109,263 @@ function Path({ fieldPath, schema }) {
 const itemRenderers = {
   'string': StringItem,
   'object': ObjectItem,
-  'rich-text': UnsupportedTypeItem,
+  'rich-text': RichTextItem,
   default: UnsupportedTypeItem,
 }
 
 function HistoryItemBody({ historyItem, schema }) {
   const renderer = itemRenderers[historyItem.details.fieldType] || itemRenderers.default
   return renderer({ historyItem, schema })
+}
+
+RichTextItem.style = css`
+
+  & .history-insertion {
+    background-color: lightgreen;
+  }
+
+  & .history-deletion {
+    background-color: lightcoral;
+    text-decoration: line-through;
+  }
+
+  & .history-mark-added, & .history-mark-removed {
+    background-color: khaki;
+  }
+`
+
+function RichTextItem({ historyItem }) {
+console.log(historyItem)
+console.log('before', historyItem.details.steps)
+
+  // TODO: this should be stored with the history item
+  const schema = context.documentSchemas[0].fields[3].schema
+
+  // merge steps
+  let lastStep = null
+  let steps = []
+  for (const jsonStep of historyItem.details.steps) {
+    const step = Step.fromJSON(schema, jsonStep)
+    if (!lastStep) {
+      lastStep = step
+      continue
+    }
+
+    const mergedStep = lastStep.merge(step)
+    if (mergedStep) {
+      lastStep = mergedStep
+      continue
+    }
+
+    steps.push(lastStep)
+    lastStep = step
+  }
+  steps.push(lastStep)
+  console.log('after steps', steps.map(x => x.toJSON()))
+
+  // apply steps and create mapping
+  console.log('before doc', historyItem.details.oldValue)
+  const initialDoc = Node.fromJSON(schema, historyItem.details.oldValue
+    ? historyItem.details.oldValue
+    : { type: 'doc', content: [] }
+  )
+  let currentDoc = initialDoc
+  const mapping = new Mapping()
+  /**
+   * @type {Array<
+   *   { type: 'insert', step: ReplaceStep, deletedSlice: null } |
+   *   { type: 'delete', step: ReplaceStep, deletedSlice: any } |
+   *   { type: 'replace', step: ReplaceStep, deletedSlice: any } |
+   *   { type: 'addMark', step: AddMarkStep, deletedSlice: null } |
+   *   { type: 'removeMark', step: RemoveMarkStep, deletedSlice: null } |
+   *   { type: 'unknown', step: Step, deletedSlice: null }
+   * >}
+   * */
+  const stepMetadata = []
+
+  for (const step of steps) {
+    let deletedSlice = null
+    if (step instanceof ReplaceStep && step.from < step.to) {
+      deletedSlice = currentDoc.slice(step.from, step.to)
+    }
+
+    let result = null
+    try {
+      result = step.apply(currentDoc)
+    } catch (e) {
+      result = { failed: 'error applying step, most likely caused by incomplete information' }
+    }
+
+    if (result.failed) {
+      console.error(`Failed to apply step: "${result.failed}":\n${JSON.stringify(step.toJSON(), null, 2)}`)
+      break
+    }
+
+    // @ts-ignore
+    stepMetadata.push({
+      step,
+      deletedSlice,
+      type: (
+        step instanceof ReplaceStep && step.slice.size > 0 && step.from === step.to ? 'insert' :
+        step instanceof ReplaceStep && step.slice.size === 0 && step.from < step.to ? 'delete' :
+        step instanceof ReplaceStep ? 'replace' :
+        step instanceof AddMarkStep ? 'addMark' :
+        step instanceof RemoveMarkStep ? 'removeMark' :
+        'unknown'
+      ),
+    })
+    currentDoc = result.doc
+    mapping.appendMap(step.getMap())
+  }
+
+  console.log('after doc', currentDoc.toJSON())
+  console.log('after doc original', historyItem.details.newValue)
+
+  console.log(stepMetadata)
+
+  // add decorations for
+  // - add
+  // - remove (crossed through text)
+  // - change
+  // - mark add
+  // - mark remove
+  const decorations = []
+  for (const [i, { type, step, deletedSlice }] of stepMetadata.entries()) {
+
+    console.log({ type })
+    const mappingForStep = mapping.slice(i)
+    if (type === 'insert') {
+      const mapResult = mappingForStep.mapResult(step.from)
+      if (mapResult.pos >= 0) {
+        const mappedFrom = mapResult.pos
+        const mappedTo = mappedFrom + step.slice.size
+        decorations.push(
+          Decoration.inline(mappedFrom, mappedTo, {
+            class: 'history-insertion',
+          })
+        )
+      } else {
+        console.warn(`Can not display insertion, location was invalid`, step.slice.content)
+      }
+    }
+    else if (type === 'delete') {
+      const deletedText = deletedSlice.content.textBetween(0, deletedSlice.content.size)
+      const mapResult = mappingForStep.mapResult(step.from)
+      if (mapResult.pos >= 0) {
+        const mappedFrom = mapResult.pos
+        decorations.push(
+          Decoration.widget(
+            mappedFrom,
+            () => {
+              const span = document.createElement('span')
+              span.textContent = deletedText || '[empty]'
+              span.className = 'history-deletion'
+              return span
+            },
+            { side: -1, marks: [] }
+          )
+        )
+      } else {
+        console.warn(`Can not display deletion, location was invalid`, `"${deletedText}"`)
+      }
+    }
+    else if (type === 'replace') {
+      const mapResult = mappingForStep.mapResult(step.from)
+      const deletedText = deletedSlice.content.textBetween(0, deletedSlice.content.size)
+      if (mapResult.pos >= 0) {
+        const mappedFrom = mapResult.pos
+        const mappedTo = mappedFrom + step.slice.size
+        decorations.push(
+          Decoration.widget(
+            mappedFrom,
+            () => {
+              const span = document.createElement('span')
+              span.textContent = deletedText || '[empty]'
+              span.className = 'history-deletion'
+              return span
+            },
+            { side: -1, marks: [] }
+          )
+        )
+        decorations.push(
+          Decoration.inline(mappedFrom, mappedTo, {
+            class: 'history-insertion',
+          })
+        )
+      } else {
+        console.warn(`Can not display replace, location was invalid`, `"${deletedText}"`)
+      }
+    }
+    else if (type === 'addMark') {
+        const mapResultFrom = mappingForStep.mapResult(step.from)
+        const mapResultTo = mappingForStep.mapResult(step.to)
+        if (!mapResultFrom.deleted && !mapResultTo.deleted) {
+            decorations.push(
+                Decoration.inline(mapResultFrom.pos, mapResultTo.pos, {
+                    class: 'history-mark-added',
+                    title: `Added ${step.mark.type.name}`,
+                    'data-mark-type': step.mark.type.name,
+                })
+            )
+        }
+    }
+    else if (type === 'removeMark') {
+        const mapResultFrom = mappingForStep.mapResult(step.from)
+        const mapResultTo = mappingForStep.mapResult(step.to)
+        if (!mapResultFrom.deleted && !mapResultTo.deleted) {
+             decorations.push(
+                Decoration.inline(mapResultFrom.pos, mapResultTo.pos, {
+                    class: 'history-mark-removed',
+                    title: `Remove ${step.mark.type.name}`,
+                    'data-mark-type': step.mark.type.name,
+                })
+            )
+        }
+    } else {
+      console.warn(`Unknown step type '${type}':\n${JSON.stringify(step.toJSON(), null, 2)}`)
+    }
+  }
+
+  console.log(new Date(historyItem.timestampStart).toISOString(), decorations.slice())
+
+  // create prosemirror renderer that returns a dom node
+  const decorationSet = DecorationSet.create(currentDoc, decorations)
+
+  const pluginKey = new PluginKey('decorations')
+  const editorState = EditorState.create({
+    doc: currentDoc,
+    plugins: [
+      new Plugin({
+        key: pluginKey,
+        state: {
+          init() {
+            return decorationSet
+          },
+          apply(tr, oldDecorationSet) {
+            return oldDecorationSet
+          }
+        },
+        props: {
+          decorations(state) {
+            console.log(new Date(historyItem.timestampStart).toISOString(), pluginKey.getState(state))
+            return pluginKey.getState(state)
+          },
+          editable(state) {
+            return false
+          }
+        }
+      })
+    ]
+  })
+
+  const view = new EditorView(null, { state: editorState })
+
+  // render dom node in raw, just as in RichTextEditor.js
+  return (
+    div({ css: RichTextItem.style },
+      raw(view.dom),
+    )
+  )
 }
 
 function UnsupportedTypeItem({ historyItem, schema }) {
